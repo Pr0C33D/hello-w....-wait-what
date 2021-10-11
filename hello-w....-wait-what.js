52676dbcbf3cf5fa0b48694be267eb30eeeacddc
Pr0C33D <hero1393@gmail.com>
2021-10-11 12:18:05 +0300

    DONE

---
+++ hello-w....-wait-what.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/54db3f2903e88ad6c30002ff
+
+let helloWorld = function () {
+    // Hello World!
+    let start = [,,,,,,,,,,,,,,,,,,,].length,
+        end = [,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,].length;
+    return helloWorld.toString().slice(start, end);
+  }
